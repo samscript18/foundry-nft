9362972105e4d3081194f18e3b064bdc459aa354
samscript18 <adeolasam20@gmail.com>
2026-03-26 07:24:24 +0100

    feat: more tests & script for minting, flipping moodNft

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,13 +5,18 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "src/BasicNft.sol";
+import {MoodNft} from "src/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
         mintNftOnContract(mostRecentlyDeployed);
     }
 
@@ -19,5 +24,52 @@ contract MintBasicNft is Script {
         vm.startBroadcast();
         BasicNft(contractAddress).mintNft(PUG_URI);
         vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        string memory sadSvg = vm.readFile("./images/sad.svg");
+        string memory happySvg = vm.readFile("./images/happy.svg");
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        moodNft.mintNft();
+        vm.stopBroadcast();
+    }
+
+    function svgToImageURI(string memory svg) public returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
+
+contract FlipMoodNft is Script {
+    function run() external {
+        string memory sadSvg = vm.readFile("./images/sad.svg");
+        string memory happySvg = vm.readFile("./images/happy.svg");
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        moodNft.mintNft();
+        moodNft.flipMood(0);
+        vm.stopBroadcast();
+    }
+
+    function svgToImageURI(string memory svg) public returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 }
